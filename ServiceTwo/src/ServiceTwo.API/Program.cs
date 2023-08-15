1548bcd17ef04b095fc43aca9184c4b966921d68
Dmytro Semenko <45269867+DmytroSemenko@users.noreply.github.com>
2023-08-16 02:54:10 +0300

    Add project files

---
+++ ServiceTwo/src/ServiceTwo.API/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => Test.TestMethod());
+
+app.Run();
+
+
+static class Test
+{
+    public static int Counter = 0;
+
+    public static string TestMethod()
+    {
+        return $"{Counter++} ServiceTwo API";
+    }
+}
